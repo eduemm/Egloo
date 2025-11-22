d0b7ad0b3f2610ac82a9da5b0b2cede937336593
eduemm <eduardoemmanuello1995@gmail.com>
2025-11-22 19:31:08 +0100

    header

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+// Seleziono il pulsante lingua
+const languageBtn = document.querySelector('.header__language button');
+
+languageBtn.addEventListener('click', () => {
+    // Logica per cambio lingua
+    alert('Cambio lingua cliccato!');
+});
+
+// Eventuali funzioni future per header, menu, scroll, ecc.
